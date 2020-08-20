338c26ffc48ca7a96e5f09a33b516fb5879f4c5c
John Letey <johnletey@gmail.com>
2020-08-20 17:44:11 +0100

    chore: Fmt

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-require("dotenv").config()
-const Discord = require("discord.js")
-const client = new Discord.Client()
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-const Arweave = require("arweave")
-const Community = require('community-js').default
+const Arweave = require("arweave");
+const Community = require("community-js").default;
 
 client.on("ready", async () => {
-  console.log(`[discord] Logged in as ${client.user.tag}!`)
+  console.log(`[discord] Logged in as ${client.user.tag}!`);
 
-  let community = new Community(new Arweave({
-    host: "arweave.net",
-    port: 443,
-    protocol: "https",
-    timeout: 20000,
-  }));
+  let community = new Community(
+    new Arweave({
+      host: "arweave.net",
+      port: 443,
+      protocol: "https",
+      timeout: 20000,
+    })
+  );
 
   await community.setCommunityTx(process.env.COMMUNITY);
 
-  let votes = (await community.getState()).votes
-  console.log(votes)
-})
+  let votes = (await community.getState()).votes;
+  console.log(votes);
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
