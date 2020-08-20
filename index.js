885e8ec678e5ce0f096a9aa4b4f218313aeaeb54
John Letey <johnletey@gmail.com>
2020-08-20 17:43:48 +0100

    feat: Grab votes from community-js

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ require("dotenv").config()
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+const Arweave = require("arweave")
+const Community = require('community-js').default
+
+client.on("ready", async () => {
+  console.log(`[discord] Logged in as ${client.user.tag}!`)
+
+  let community = new Community(new Arweave({
+    host: "arweave.net",
+    port: 443,
+    protocol: "https",
+    timeout: 20000,
+  }));
+
+  await community.setCommunityTx(process.env.COMMUNITY);
+
+  let votes = (await community.getState()).votes
+  console.log(votes)
 })
 
 client.login(process.env.BOT_TOKEN)
