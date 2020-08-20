0cd984771cde8391414eb117bc4f66a608e27041
John Letey <johnletey@gmail.com>
2020-08-20 17:58:48 +0100

    feat: Send list of votes

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const Community = require("community-js").default;
 client.on("ready", async () => {
   console.log(`[discord] Logged in as ${client.user.tag}!`);
 
+  const channel = client.channels.cache.find(
+    (channel) => channel.name === process.env.CHANNEL
+  );
+
   let community = new Community(
     new Arweave({
       host: "arweave.net",
@@ -20,7 +24,7 @@ client.on("ready", async () => {
   await community.setCommunityTx(process.env.COMMUNITY);
 
   let votes = (await community.getState()).votes;
-  console.log(votes);
+  channel.send("```\n" + JSON.stringify(votes, null, 2) + "\n```");
 });
 
 client.login(process.env.BOT_TOKEN);
