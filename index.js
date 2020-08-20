6acd030255a157fbcf802c587a913e43f71440a9
John Letey <johnletey@gmail.com>
2020-08-20 17:05:25 +0100

    feat: Connect to Discord

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(process.env.BOT_TOKEN)
