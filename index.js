e986cae7f12bbbca7acd54c05ca640e30b4a3cd4
John Letey <johnletey@gmail.com>
2020-08-21 13:53:54 +0100

    fix: Properly cache and cleanup

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ const client = new Discord.Client();
 const Arweave = require("arweave");
 const Community = require("community-js").default;
 
+let cachedLength = 0;
+let community = new Community(
+  new Arweave({
+    host: "arweave.net",
+    port: 443,
+    protocol: "https",
+    timeout: 20000,
+  })
+);
+
 client.on("ready", async () => {
-  console.log(`[discord] Logged in as ${client.user.tag}!`);
-
-  let cachedLength = 0;
-
   const channel = client.channels.cache.find(
     (channel) => channel.name === process.env.CHANNEL
-  );
-
-  let community = new Community(
-    new Arweave({
-      host: "arweave.net",
-      port: 443,
-      protocol: "https",
-      timeout: 20000,
-    })
   );
 
   await community.setCommunityTx(process.env.COMMUNITY);
@@ -28,9 +25,8 @@ client.on("ready", async () => {
   setInterval(async () => await checkVotes(community, cachedLength, channel), 1000 * 60);
 });
 
-async function checkVotes(community, cachedLength, channel) {
-  let votes = (await community.getState()).votes;
-  console.log(votes);
+async function checkVotes(channel) {
+  let votes = (await community.getState(false)).votes;
 
   if (votes.length > cachedLength) {
     for (let i = cachedLength; i < votes.length; i++) {
